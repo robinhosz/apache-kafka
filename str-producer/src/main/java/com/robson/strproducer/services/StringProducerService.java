dc152721fe44b199fd8eacd49efd86964db164c4
José Robson <joserobsonsiqueira23@hotmail.com>
2022-10-31 15:02:57 -0300

    feat: implements callback function to sendMessage function

--- str-producer/src/main/java/com/robson/strproducer/services/StringProducerService.java
+++ str-producer/src/main/java/com/robson/strproducer/services/StringProducerService.java
@@ -1,9 +1,11 @@
 package com.robson.strproducer.services;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+@Log4j2
 @RequiredArgsConstructor
 @Service
 public class StringProducerService {
@@ -11,6 +13,15 @@ public class StringProducerService {
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String messsage) {
-        kafkaTemplate.send("str-topic", messsage);
+        kafkaTemplate.send("str-topic", messsage).addCallback(
+                sucess -> {
+                    if(sucess != null) {
+                        log.info("Send message with sucess {}", messsage);
+                        log.info("Partition {}, Offset {}", sucess.getRecordMetadata().partition(),
+                                sucess.getRecordMetadata().offset());
+                    }
+                },
+                error -> log.error("Error send message")
+        );
     }
 }
